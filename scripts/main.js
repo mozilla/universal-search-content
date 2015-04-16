80267f44f0e4634344b3f50621ffb27b03c6a0d3
Torgeir Hovden <torgeir@urbanexpression.net>
2015-04-16 19:17:49 +0200

    Removed console log

--- scripts/main.js
+++ scripts/main.js
@@ -205,7 +205,6 @@ var autocompleteSearchResultsView = new AutocompleteSearchResultsView({ el: $('#
 
 // Listen for key events from the chrome
 window.addEventListener('WebChannelMessageToContent', function (event) {
-  console.log("Got message", event.detail.message);
   var message = event.detail.message;
 
   if (message.type === 'navigational-key' && message.data) {
