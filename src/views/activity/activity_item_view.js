26496d5482b3934ff8048a0ecda4136e3b4e3bba
Chuck Harmston <chuck@chuckharmston.com>
2015-12-03 06:12:10 -0700

    Move favicon event delegation into views (closes #99).

--- src/views/activity/activity_item_view.js
+++ src/views/activity/activity_item_view.js
@@ -1,5 +1,6 @@
 import BaseView from '../base_view';
 import ActivityItemView from '../../templates/activity/item.html';
+import Favicon from '../../lib/image_colors';
 import webChannel from '../../lib/web_channel';
 
 export default BaseView.extend({
@@ -8,6 +9,11 @@ export default BaseView.extend({
   events: {
     'mousedown': 'openUrl',
     'select': 'sendSelectionDetails'
+  },
+
+  afterRender () {
+    this.iconContainer = this.query('.icon.favicon');
+    this.injectFavicon();
   },
 
   openUrl (event) {
@@ -21,5 +27,31 @@ export default BaseView.extend({
 
   sendSelectionDetails (event) {
     webChannel.sendUrlSelected(this.model.url, 'url');
+  },
+
+  injectFavicon () {
+    // Manually construct the favicon element and inject it into the DOM,
+    // allowing us to handle events directly, since load events don't bubble
+    // and cannot be delegated.
+    this.icon = new Image('img');
+    this.icon.onload = () => { this.calculateIconColor(); };
+    this.icon.onerror = () => { this.hideErroredFavicon(); };
+    this.icon.src = this.model.displayImage();
+    this.iconContainer.appendChild(this.icon);
+  },
+
+  hideErroredFavicon () {
+    this.icon.style.display = 'none';
+  },
+
+  calculateIconColor () {
+    // If able to do so in 20ms or less, calculate the dominant color of the
+    // favicon and set that to the background color of its parent.
+    const START = performance.now();
+    const favicon = new Favicon(this.icon);
+    const END = performance.now();
+    if (favicon.dominantColor && END - START < 20) {
+      this.iconContainer.style.backgroundColor = favicon.dominantColor;
+    }
   }
 });
